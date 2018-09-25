01ae0a3573899498df3caf75bf610b6c311d7d74
Alex <alex.weininger@live.com>
2018-09-24 18:01:41 -0700

    commenting and format

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-// Alex Weininger 2018
+/** Script.js 
+ * 
+ * Contains the main program logic for recent-pdf
+ *  - loads pdf files from downloads api
+ * 
+ *  author: Alex Weininger
+ *  last modified: 9/24/2018
+ */
 
 let localPdfCount = 0;
 let onlineCount = 0;
+
 let element = document.getElementById('link-list');
 let fileElement = document.getElementById('file-list');
 
-let doneLoading = 0;
+let maxLocalFilesPerPage = 11; // setting; set to 11 by default
+
+// load the user settings
+loadSettings();
 
 searchHistory();
 
@@ -43,17 +54,10 @@ function searchHistory() {
                     icon.classList.add('link-thumb');
                     icon.src = `chrome://favicon/${page.url}`;
 
-                    // listItem.innerHTML = strA;
                     leftDiv.appendChild(icon);
                     leftDiv.appendChild(title);
                     leftDiv.appendChild(document.createElement('br'));
                     leftDiv.appendChild(linkUrl);
-
-                    // let more = document.createElement('img');
-                    // more.id = 'more_icon';
-                    // more.src = '../../assets/More.png';
-
-                    //rightDiv.appendChild(more);
 
                     leftDiv.addEventListener('click', function () {
                         window.open(page.url);
@@ -66,20 +70,12 @@ function searchHistory() {
             }
         });
 
-        doneLoading++;
-
-        // let plural = (onlineCount > 1 ? 's' : '');
-
-        // let onlineFooter = document.createElement('p');
-        // onlineFooter.innerHTML = `Showing ${onlineCount} online PDF${plural}.`;
-        // onlineFooter.classList.add('footer');
-        // onlineFooter.id = 'online-footer';
-        // element.appendChild(onlineFooter);
-
         searchDownloads();
         console.log(`${onlineCount} online PDFs found.`);
     });
 }
+
+let localFiles = [];
 
 function searchDownloads() {
     chrome.downloads.search({
@@ -88,60 +84,67 @@ function searchDownloads() {
     }, function (data) {
         data.forEach(function (file, i) {
             if (file.filename.endsWith('.pdf')) {
-                localPdfCount++;
 
-                let leftDiv = document.createElement('div');
-                let rightDiv = document.createElement('div');
-                leftDiv.classList.add('list-div', 'left');
-                rightDiv.classList.add('list-div', 'right');
 
-                let fileItem = document.createElement("li");
-                fileItem.classList.add('list-item', 'file-item');
+                if (!localFiles.includes(file.filename) && localPdfCount < maxLocalFilesPerPage) {
+                    localFiles.push(file.filename);
 
-                let icon = document.createElement('img');
-                icon.classList.add('link-thumb');
-                chrome.downloads.getFileIcon(file.id, {
-                    size: 16
-                }, function (iconUrl) {
-                    icon.src = iconUrl;
-                });
 
-                let title = document.createElement('p');
-                title.classList.add('link-title');
-                title.innerText = file.filename.substring(file.filename.lastIndexOf('\\') + 1, file.filename.length - 4);
+                    localPdfCount++;
 
-                let linkUrl = document.createElement('p');
-                linkUrl.classList.add('link-url');
-                linkUrl.innerHTML = file.filename.substring(0, file.filename.lastIndexOf('\\') + 1);
+                    let leftDiv = document.createElement('div');
+                    let rightDiv = document.createElement('div');
+                    leftDiv.classList.add('list-div', 'left');
+                    rightDiv.classList.add('list-div', 'right');
 
-                leftDiv.appendChild(icon);
-                leftDiv.appendChild(title);
-                leftDiv.appendChild(document.createElement('br'));
-                leftDiv.appendChild(linkUrl);
+                    let fileItem = document.createElement('li');
+                    fileItem.classList.add('list-item', 'file-item');
 
-                leftDiv.addEventListener('click', function () {
-                    chrome.downloads.open(file.id);
-                });
+                    let icon = document.createElement('img');
+                    icon.classList.add('link-thumb');
+                    chrome.downloads.getFileIcon(file.id, {
+                        size: 16
+                    }, function (iconUrl) {
+                        icon.src = iconUrl;
+                    });
 
-                let more = document.createElement('img');
-                more.id = 'more_icon';
-                more.src = '../../assets/More.png';
-                more.addEventListener('click', function () {
-                    chrome.downloads.show(file.id);
-                });
+                    let title = document.createElement('p');
+                    title.classList.add('link-title');
+                    title.innerText = file.filename.substring(file.filename.lastIndexOf('\\') + 1, file.filename.length - 4);
 
-                rightDiv.appendChild(more);
+                    let linkUrl = document.createElement('p');
+                    linkUrl.classList.add('link-url');
+                    linkUrl.innerHTML = file.filename.substring(0, file.filename.lastIndexOf('\\') + 1);
 
-                fileItem.appendChild(leftDiv);
-                fileItem.appendChild(rightDiv);
-                fileElement.appendChild(fileItem);
+                    leftDiv.appendChild(icon);
+                    leftDiv.appendChild(title);
+                    leftDiv.appendChild(document.createElement('br'));
+                    leftDiv.appendChild(linkUrl);
+
+                    leftDiv.addEventListener('click', function () {
+                        chrome.downloads.open(file.id);
+                    });
+
+                    let more = document.createElement('img');
+                    more.id = 'more_icon';
+                    more.src = '../../assets/More.png';
+                    more.addEventListener('click', function () {
+                        chrome.downloads.show(file.id);
+                    });
+
+                    rightDiv.appendChild(more);
+
+                    fileItem.appendChild(leftDiv);
+                    fileItem.appendChild(rightDiv);
+                    fileElement.appendChild(fileItem);
+                } else {
+                    console.log(`skipped duplicate file: ${file.filename}.`);
+                }
             }
         });
 
         console.log(`${localPdfCount} local PDFs found.`);
 
-        //localFooter();
-        doneLoading++;
         loadSettings();
         footer(2);
     });
@@ -156,13 +159,13 @@ function footer(count) {
     let countDisplay = document.createElement('p');
     countDisplay.id = 'count-display';
     countDisplay.innerHTML = 'placeholder for count display';
-    
+
     footerLeft.appendChild(countDisplay);
 
     let settingsIcon;
     for (e of footerDivs) {
         console.log(e);
-        
+
     }
 }
 
@@ -199,23 +202,27 @@ onlineTabLink.click();
 // function that handles switching between tabs
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName('tabcontent');
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = 'none';
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName('tablinks');
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(' active', '');
     }
-    document.getElementById(tabName).style.display = "inline-block";
-    evt.currentTarget.className += " active";
+    document.getElementById(tabName).style.display = 'inline-block';
+    evt.currentTarget.className += ' active';
 }
 
 function loadSettings() {
-    chrome.storage.sync.get(['savedTab'], function (result) {
+    chrome.storage.sync.get(['savedTab', 'filesPerPage'], function (result) {
         console.log(result);
         if (result.savedTab) {
             document.getElementById('online-footer').style = 'color: red';
+        }
+
+        if (result.filesPerPage) {
+            maxLocalFilesPerPage = result.filesPerPage;
         }
     });
 }
