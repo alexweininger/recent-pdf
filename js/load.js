433be9ba6b2cec110576a9606ec5317dc8d1ee24
Alex <alex.weininger@live.com>
2018-09-28 10:54:17 -0700

    cleaned up the callback function for the downloads.search call

--- js/load.js
+++ js/load.js
@@ -13,22 +13,32 @@
  * 3. go through history
  */
 
-var pdfList = [];
+// regex for matching pdfs only, can be used with search function of chrome.downloads
+// .+\.([pP][dD][fF])
+<reference path="chrome-api-vsdoc.js"/>
+
+var pdfList = []; // list of all pdf objects
 
 function getDownloads() {
 	chrome.downloads.search({
+		urlRegex: 
 		limit: 1000,
 		orderBy: ['-startTime']
-	}, function (data) {
+	}, (data) => {
 		data.forEach(function (page) {
 
 			if (page.url.endsWith('.pdf')) { // check if page is a .pdf
 
 				if (!pdfList.includes(page)) { // check if page already in list
 					pdfList.push(page); // add it to the array list
-					console.log(`${page.url}`);
+
+					if (page.finalUrl === page.url) {
+						console.log(`[INFO] url and final url do not match\nurl: ${page.url}, finalUrl: ${page.finalUrl}`);
+					}
+
+					
 				}
 			}
 		});
-	});
+	})
 }
