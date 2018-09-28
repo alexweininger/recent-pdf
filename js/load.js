21f6bc4cf4fc39c6ebd724e6e5a663b18dc2db3c
Alex <alex.weininger@live.com>
2018-09-28 11:04:54 -0700

    removed completely from all files

--- js/load.js
+++ js/load.js
@@ -1,7 +1,7 @@
 /**
  * load.js
  * loads all pdfs from chrome apis
- * 
+ *
  * author: Alex Weininger
  * modified: 9/26/2018
  */
@@ -13,31 +13,29 @@
  * 3. go through history
  */
 
-// regex for matching pdfs only, can be used with search function of chrome.downloads
+// regex for matching pdfs only, can be used with search function of
+// chrome.downloads
 // .+\.([pP][dD][fF])
 
-	var pdfList = []; // list of all pdf objects
+var pdfList = [];  // list of all pdf objects
 
 function getDownloads() {
-	chrome.downloads.search({
-		urlRegex: '.+\.([pP][dD][fF])',
-		limit: 1000,
-		orderBy: ['-startTime']
-	}, (data) => {
-		data.forEach(function (page) {
+  chrome.downloads.search(
+      {urlRegex: '.+\.([pP][dD][fF])', limit: 1000, orderBy: ['-startTime']},
+      (data) => {
+        data.forEach(function(page) {
 
-			//if (page.url.endsWith('.pdf')) { // check if page is a .pdf
+          // if (page.url.endsWith('.pdf')) { // check if page is a .pdf
 
-			if (!pdfList.includes(page)) { // check if page already in list
-				pdfList.push(page); // add it to the array list
+          if (!pdfList.includes(page)) {  // check if page already in list
+            pdfList.push(page);           // add it to the array list
 
-				if (page.finalUrl === page.url) {
-					console.log(`[INFO] url and final url do not match\nurl: ${page.url}, finalUrl: ${page.finalUrl}`);
-				}
-
-
-			}
-			//	}
-		});
-	})
+            if (page.finalUrl === page.url) {
+              console.log(
+                  `[INFO] url and final url do not match\nurl: ${page.url}, finalUrl: ${page.finalUrl}`);
+            }
+          }
+          //	}
+        });
+      })
 }
