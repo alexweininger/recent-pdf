6496589047cf2dd2384eb78ced9fccaa94c64a95
alexweininger <alex.weininger@live.com>
2018-09-29 01:44:27 -0700

    info logging in load.js

--- js/load.js
+++ js/load.js
@@ -7,25 +7,37 @@
  */
 
 function getDownloads() {
-	let pdfList = []
-	chrome.downloads.search({
-			urlRegex: '.+\.([pP][dD][fF])', // regex for .pdf files
-			limit: 500,
-			orderBy: ['-startTime']
-		},
-		(data) => {
-			data.forEach(function (page) {
-				if (!pdfList.includes(page)) { // check if page already in list
-					
-					pdfList.push(page) // add it to the array list
+  let pdfList = []
+  chrome.downloads.search({
+      urlRegex: '/.+\.([pP][dD][fF])/g', // regex for .pdf files
+      limit: 500,
+      orderBy: ['-startTime']
+    },
+    (data) => {
+      data.forEach(function (page) {
 
-					if (page.finalUrl != page.url) {
-						console.log(`[INFO] url and final url do not match`)
-						console.log(` > url: ${page.url}, finalUrl: ${page.finalUrl}`)
-					}
-				} else {
-					console.info(`Duplicate file not pushed to file list.`);
-				}
-			})
-		})
+        if((page.url).endsWith('.pdf')) {
+          console.error('Pushed file to list which does not end in \'.pdf\'')
+          console.groupCollapsed('url')
+          console.error(`url: ${page.url}`)
+          console.groupEnd()
+        }
+
+        if (!pdfList.includes(page)) { // check if page already in list
+
+          pdfList.push(page) // add it to the array list
+
+          if (page.finalUrl != page.url) {
+            console.info(`[INFO] url and final url do not match`)
+            console.info(` > url: ${page.url}, finalUrl: ${page.finalUrl}`)
+          }
+
+        } else {
+          console.info(`Duplicate file not pushed to file list.`)
+          console.groupCollapsed('url')
+          console.info(page.url)
+          console.groupEnd()
+        }
+      })
+    })
 }
