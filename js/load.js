e2c96eebe35a49f31e9f387315ce84271a3dc512
alexweininger <alex.weininger@live.com>
2018-09-29 01:09:08 -0700

    removed xtra comments

--- js/load.js
+++ js/load.js
@@ -6,34 +6,25 @@
  * modified: 9/26/2018
  */
 
-/**
- * steps:
- * 1. create data structure to hold pdf objects
- * 2. go through downloads and push pdfs to the list
- * 3. go through history
- */
-
-// regex for matching pdfs only, can be used with search function of
-// chrome.downloads
-// .+\.([pP][dD][fF])
-
-var pdfList = [] // list of all pdf objects
-
 function getDownloads() {
+	let pdfList = []
 	chrome.downloads.search({
-			urlRegex: '.+\.([pP][dD][fF])',
-			limit: 1000,
+			urlRegex: '.+\.([pP][dD][fF])', // regex for .pdf files
+			limit: 500,
 			orderBy: ['-startTime']
 		},
 		(data) => {
 			data.forEach(function (page) {
-
 				if (!pdfList.includes(page)) { // check if page already in list
+					
 					pdfList.push(page) // add it to the array list
+
 					if (page.finalUrl != page.url) {
 						console.log(`[INFO] url and final url do not match`)
-						console.log(`url: ${page.url}, finalUrl: ${page.finalUrl}`)
+						console.log(` > url: ${page.url}, finalUrl: ${page.finalUrl}`)
 					}
+				} else {
+					console.info(`Duplicate file not pushed to file list.`);
 				}
 			})
 		})
