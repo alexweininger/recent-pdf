86620200e2b9351b33e634158adbc725c54b4650
alexweininger <alex.weininger@live.com>
2018-09-29 01:11:29 -0700

    removed extra comments

--- js/load.js
+++ js/load.js
@@ -6,17 +6,6 @@
  * modified: 9/26/2018
  */
 
-/**
- * steps:
- * 1. create data structure to hold pdf objects
- * 2. go through downloads and push pdfs to the list
- * 3. go through history
- */
-
-// regex for matching pdfs only, can be used with search function of
-// chrome.downloads
-// .+\.([pP][dD][fF])
-
 var pdfList = [] // list of all pdf objects
 
 function getDownloads() {
