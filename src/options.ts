3e3bcf02d7d125c5d1afffd085c368354ca9e43e
alexweininger <alex.weininger@live.com>
2019-03-23 13:35:52 -0700

    refactor

--- src/options.ts
+++ src/options.ts
@@ -1,35 +1,35 @@
 /// <reference path='../node_modules/@types/chrome/index.d.ts'/>
-'../node_modules/@types/chrome/index.d.ts'
-'use strict'
+"../node_modules/@types/chrome/index.d.ts";
 // Saves options to chrome.storage
-function save_options() {
-	var isSavedTab = document.getElementById('saved-tab').nodeValue;
-	var filesPerPage = document.getElementById('files-per-page').nodeValue;
-	chrome.storage.sync.set({
-		savedTab: isSavedTab,
-		filesPerPage: filesPerPage
-	}, function () {
-		// Update status to let user know options were saved.
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		setTimeout(function () {
-			status.textContent = '';
-		}, 750);
-	});
+function save_options(): any {
+	var defaultTab: HTMLCollectionOf<HTMLOptionElement> = (<HTMLSelectElement> document.getElementById("default-tab")).selectedOptions;
+	console.log(defaultTab[0].value);
+
+    chrome.storage.sync.set(
+        {
+            defaultTab: defaultTab[0].value
+        },
+        function() {
+            // Update status to let user know options were saved.
+            var status = document.getElementById("status");
+            status.textContent = "Options saved.";
+        }
+	);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-	// Use default value color = 'red' and likesColor = true.
-	chrome.storage.sync.get({
-		savedTab: false,
-		darkTheme: false,
-		filesPerPage: 11
-	}, function (items) {
-		document.getElementById('saved-tab').nodeValue = items.savedTab;
-		document.getElementById('files-per-page').nodeValue = items.filesPerPage;
-	});
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get(
+        {
+            defaultTab: "local"
+        },
+        function(items) {}
+    );
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", (event) => {
+	event.preventDefault();
+	save_options();
+});
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,41 @@
-"use strict";
 /// <reference path='../node_modules/@types/chrome/index.d.ts'/>
-let onlineList = document.getElementById("link-list"); // online file list
-let fileElement = document.getElementById("file-list"); // offline (local) file list
-loadSettings(); // load the user settings
+
+let onlineList: HTMLUListElement = <HTMLUListElement>document.getElementById("link-list"); // online file list
+let fileElement: HTMLUListElement = <HTMLUListElement>document.getElementById("file-list"); // offline (local) file list
+let onlineTabLink: HTMLButtonElement = <HTMLButtonElement>document.getElementById("online-tab-link");
+let localTabLink: HTMLButtonElement = <HTMLButtonElement>document.getElementById("local-tab-link");
+let settingsTabLink: HTMLButtonElement = <HTMLButtonElement>document.getElementById("settings-link");
+
+// tab buttons
+
+if (onlineTabLink) {
+    // event handlers for tab buttons
+    onlineTabLink.addEventListener("click", function(event: Event) {
+        onlineFooter(onlinePdfCount);
+        openTab(event, "online");
+    });
+} else {
+    console.error("onlineTabLink is null");
+}
+
+// click listener for local pdf tab
+if (localTabLink) {
+    localTabLink.addEventListener("click", function(event: Event) {
+        localFooter(localPdfCount);
+        openTab(event, "local");
+    });
+} else {
+    console.error("localTabLink is null");
+}
+
+// settings click listener
+settingsTabLink.addEventListener("click", function() {
+    window.open("./options.html");
+});
+
 searchHistory();
 
-let onlinePdfCount = 0; // number of online pdf files
+let onlinePdfCount: number = 0; // number of online pdf files
 /**
  * searchHistory() - searches history using the chrome.history api for online pdf files
  */
@@ -16,36 +46,36 @@ function searchHistory() {
             maxResults: 10000
         },
         function(data) {
-            data.forEach(function(page) {
+            data.forEach(function(page: chrome.history.HistoryItem) {
                 // for each result
                 if (page.url.endsWith(".pdf") || page.url.endsWith(".PDF")) {
                     // check if page is a .pdf
-                    let listItem = document.createElement("li");
+                    let listItem: HTMLLIElement = document.createElement("li");
                     listItem.classList.add("list-item");
 
                     if (!page.url.startsWith("file:")) {
                         // if not local pdf
                         onlinePdfCount++;
 
-                        let leftDiv = document.createElement("div");
-                        let rightDiv = document.createElement("div");
+                        let leftDiv: HTMLDivElement = document.createElement("div");
+                        let rightDiv: HTMLDivElement = document.createElement("div");
                         leftDiv.classList.add("list-div", "left");
                         rightDiv.classList.add("list-div", "right");
 
                         // make title element
-                        let title = document.createElement("p");
+                        let title: HTMLParagraphElement = document.createElement("p");
                         title.classList.add("link-title");
                         title.innerText = decodeURI(page.url).substring(page.url.lastIndexOf("/") + 1, page.url.length - 4);
 
                         // make url element
-                        let linkUrl = document.createElement("p");
+                        let linkUrl: HTMLParagraphElement = document.createElement("p");
                         linkUrl.classList.add("link-url");
                         linkUrl.innerHTML = decodeURI(page.url)
                             .substring(0, 50)
                             .replace(" ", "");
 
                         // make icon element
-                        let icon = document.createElement("img");
+                        let icon: HTMLImageElement = document.createElement("img");
                         icon.classList.add("link-thumb");
                         icon.src = `chrome://favicon/${page.url}`;
 
@@ -75,7 +105,7 @@ function searchHistory() {
 }
 
 let localFiles: any[] = [];
-let localPdfCount = 0; // number of local pdf files
+let localPdfCount: number = 0; // number of local pdf files
 /**
  * searchDownloads() - searches downloads with chrome.downloads api for local pdf files
  */
@@ -85,10 +115,10 @@ function searchDownloads() {
             limit: 1000,
             orderBy: ["-startTime"]
         },
-        function(data) {
-            data.forEach(function(file, i) {
+        function(data: chrome.downloads.DownloadItem[]) {
+            data.forEach(function(file: chrome.downloads.DownloadItem, i: number) {
                 // for each result
-                console.log("TCL: searchDownloads -> i", i);
+                // console.log("TCL: searchDownloads -> i", i);
                 if (file.filename.endsWith(".pdf") || file.filename.endsWith(".PDF")) {
                     // check if file ends with .pdf or .PDF
                     if (localFiles.indexOf(file.filename) === -1 && localPdfCount < 30) {
@@ -96,30 +126,30 @@ function searchDownloads() {
                         localFiles.push(file.filename);
                         localPdfCount++;
 
-                        let leftDiv = document.createElement("div");
-                        let rightDiv = document.createElement("div");
+                        let leftDiv: HTMLDivElement = document.createElement("div");
+                        let rightDiv: HTMLDivElement = document.createElement("div");
                         leftDiv.classList.add("list-div", "left");
                         rightDiv.classList.add("list-div", "right");
 
                         // create local file list item
-                        let fileItem = document.createElement("li");
+                        let fileItem: HTMLLIElement = document.createElement("li");
                         fileItem.classList.add("list-item", "file-item");
 
                         // create icon element
-                        let icon = document.createElement("img");
+                        let icon: HTMLImageElement = document.createElement("img");
                         icon.classList.add("link-thumb");
                         chrome.downloads.getFileIcon(file.id, { size: 16 }, iconUrl => {
                             icon.src = iconUrl;
                         });
 
                         // create title element
-                        let title = document.createElement("p");
+                        let title: HTMLParagraphElement = document.createElement("p");
                         title.classList.add("link-title");
                         title.classList.add("local-title");
                         title.innerText = file.filename.substring(file.filename.lastIndexOf("\\") + 1, file.filename.length - 4);
 
                         // create file url element
-                        let linkUrl = document.createElement("p");
+                        let linkUrl: HTMLParagraphElement = document.createElement("p");
                         linkUrl.classList.add("link-url");
                         linkUrl.innerHTML = file.filename.substring(0, 50);
 
@@ -134,7 +164,7 @@ function searchDownloads() {
                         });
 
                         // open in file explorer button
-                        let more = document.createElement("img");
+                        let more: HTMLImageElement = document.createElement("img");
                         more.id = "more_icon";
                         more.src = "../../assets/More.png";
                         more.addEventListener("click", function() {
@@ -146,80 +176,73 @@ function searchDownloads() {
                         fileItem.appendChild(rightDiv);
                         fileElement.appendChild(fileItem);
                     } else {
-                        console.log(`[INFO] skipped duplicate file: ${file.filename}.`);
+                        // console.log(`[INFO] skipped duplicate file: ${file.filename}.`);
                     }
                 }
             });
 
             console.log(`[INFO] ${localPdfCount} local PDFs found.`);
-            loadSettings(); // load settings
         }
     );
 }
 
 // load and create the online pdf footer
 function onlineFooter(count: number) {
-    let plural = count > 1 ? "s" : "";
-    let countDisplay = document.getElementById("count-display");
+    let plural: string = count > 1 ? "s" : "";
+    let countDisplay: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("count-display");
     countDisplay.innerHTML = `Showing ${count} online PDF${plural}.`;
 }
 
 // load and create the local file footer
 function localFooter(count: number) {
-    let plural = count > 1 ? "s" : "";
-    let countDisplay = document.getElementById("count-display");
+    let plural: string = count > 1 ? "s" : "";
+    let countDisplay: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("count-display");
     countDisplay.innerHTML = `Showing ${count} local PDF${plural}.`;
 }
-
-// tab buttons
-let onlineTabLink = document.getElementById("online-tab-link");
-let localTabLink = document.getElementById("local-tab-link");
-let settingsTabLink = document.getElementById("settings-link");
-
-if (onlineTabLink !== null) {
-    // event handlers for tab buttons
-    onlineTabLink.addEventListener("click", function(event) {
-        onlineFooter(onlinePdfCount);
-        openTab(event, "online");
-    });
-}
-
-// click listener for local pdf tab
-localTabLink.addEventListener("click", function(event) {
-    localFooter(localPdfCount);
-    openTab(event, "local");
-});
-
-// settings click listener
-settingsTabLink.addEventListener("click", function() {
-    window.open("./options.html");
-});
-
-// open the online tab by default
-onlineTabLink.click();
 
 // function that handles switching between tabs
 function openTab(evt: any, tabName: string) {
     // Find active elements and remove active class from elements
-    const activeElements = document.querySelectorAll(".active");
-    activeElements.forEach(function(elem) {
+    const activeElements: NodeListOf<Element> = <NodeListOf<Element>>document.querySelectorAll(".active");
+    activeElements.forEach(function(elem: HTMLElement) {
         elem.classList.remove("active");
     });
 
     // Add active class to tab and pressed button
-    const tab = document.querySelector(`.tabcontent#${tabName}`);
+    const tab: HTMLElement = <HTMLElement>document.querySelector(`.tabcontent#${tabName}`);
     if (tab) {
         tab.classList.add("active");
     }
     evt.currentTarget.classList.add("active");
 }
 
-// function that loads the settings from the options.js script
-function loadSettings() {
-    chrome.storage.sync.get(["savedTab", "filesPerPage"], function(result) {
-        console.log(result);
-        if (result.savedTab) {
-            document.getElementById("online-footer").style.color = "red";
+async function getOption(name: string): Promise<any> {
+    return await chrome.storage.sync.get([name], async (result: any) => {
+        if (result) {
+            console.log('getOption', result);
+            return result;
         }
     });
 }
+
+async function loadOptions() {
+    let defaultTab = await getOption("defaultTab");
+    console.log("setting", defaultTab);
+
+    if (defaultTab) {
+        console.log(defaultTab.defaultTab);
+
+        if (defaultTab === "online") {
+            onlineTabLink.click();
+        } else if (defaultTab === "local") {
+            localTabLink.click();
+        } else {
+            localTabLink.click();
+        }
+    } else {
+        console.log("uh oh");
+        localTabLink.click();
+    }
+}
+
+loadOptions();
