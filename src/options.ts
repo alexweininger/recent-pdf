3ef57fae041a3b34f30b5f482a8749d97e5eb5cd
Tucker <trblackw@ncsu.edu>
2019-10-10 08:10:10 -0400

    Clean up file and add proper return types for funcs

--- src/options.ts
+++ src/options.ts
@@ -1,35 +1,33 @@
 /// <reference path='../node_modules/@types/chrome/index.d.ts'/>
-"../node_modules/@types/chrome/index.d.ts";
+'../node_modules/@types/chrome/index.d.ts';
 // Saves options to chrome.storage
-function save_options(): any {
-	var defaultTab: HTMLCollectionOf<HTMLOptionElement> = (<HTMLSelectElement> document.getElementById("default-tab")).selectedOptions;
-	console.log(defaultTab[0].value);
+function save_options(): void {
+   var defaultTab: HTMLCollectionOf<HTMLOptionElement> = (<HTMLSelectElement>document.getElementById('default-tab')).selectedOptions;
+   console.log(defaultTab[0].value);
 
-    chrome.storage.sync.set(
-        {
-            defaultTab: defaultTab[0].value
-        },
-        function() {
-            // Update status to let user know options were saved.
-            var status = document.getElementById("status");
-            status.textContent = "Options saved.";
-        }
-	);
+   chrome.storage.sync.set(
+      {
+         defaultTab: defaultTab[0].value
+      },
+      () => {
+         // Update status to let user know options were saved.
+         const status = document.getElementById('status');
+         status.textContent = 'Options saved.';
+      }
+   );
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get(
-        {
-            defaultTab: "local"
-        },
-        function(items) {}
-    );
+function restore_options(): void {
+   // Use default value color = 'red' and likesColor = true.
+   chrome.storage.sync.get({
+    //@ts-ignore
+      defaultTab: 'local'
+   });
 }
-document.addEventListener("DOMContentLoaded", restore_options);
-document.getElementById("save").addEventListener("click", (event) => {
-	event.preventDefault();
-	save_options();
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', event => {
+   event.preventDefault();
+   save_options();
 });
